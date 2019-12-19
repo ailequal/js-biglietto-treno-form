962a0069cabf84c4334ab374a062dbe5948ece0c
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-19 17:12:23 +0100

    First function implemented

--- js/script.js
+++ js/script.js
@@ -3,24 +3,24 @@ var btnGen = document.getElementById('btn_gen');
 btnGen.addEventListener('click',
   function () {
     // user name and surname
-    var userNameSurname = document.getElementById('user_name_surname');
+    var userNameSurname = document.getElementById('user_name_surname').value;
     console.log("userNameSurname set to: " + userNameSurname);
     // how much km do you want to travel?
-    var travelKm = document.getElementById('travel_km');
+    var travelKm = document.getElementById('travel_km').value;
     console.log("travelKm set to: " + travelKm);
     // how old are you?
-    var userAge = document.getElementById('user_age');
+    var userAge = document.getElementById('user_age').value;
     console.log("userAge set to: " + userAge);
     // ticket pricing 0.21 euros per km
     // < 18y 20% discount
     // > 65y 40% discount
     var travelPrice;
-    if (userAge < 18) {
+    if (userAge == 'minor') {
       travelPrice = Math.ceil(travelKm * 0.21 * 0.80 * 100) / 100;
-    } else if (userAge > 65) {
+    } else if (userAge == 'over65') {
       travelPrice = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
     } else {
-      travelPrice = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
+      travelPrice = Math.ceil(travelKm * 0.21 * 1.00 * 100) / 100;
     }
     console.log("travelPrice set to: " + travelPrice);
   }
