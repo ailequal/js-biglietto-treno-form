4bae447c99ee9a933cfb8fe6a7dc08631aa54d79
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-19 15:39:37 +0100

    Imported old JS code without prompt messages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+// how much km do you want to travel?
+var travelKm = ;
+console.log("travelKm set to: " + travelKm);
+
+// how old are you?
+var travelAge = ;
+console.log("travelAge set to: " + travelAge);
+
+// ticket pricing 0.21 euros per km
+// < 18y 20% discount
+// > 65y 40% discount
+var travelPrice;
+if (travelAge < 18) {
+  travelPrice = Math.ceil(travelKm * 0.21 * 0.80 * 100) / 100;
+} else if (travelAge > 65) {
+  travelPrice = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
+} else {
+  travelPrice = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
+}
+console.log("travelPrice set to: " + travelPrice);
