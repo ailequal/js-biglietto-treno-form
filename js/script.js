9a83e6fd70c365e12a562489d70d5416c077c76c
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-19 17:35:57 +0100

    Function almost completed

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ var btnGen = document.getElementById('btn_gen');
 btnGen.addEventListener('click',
   function () {
     // user name and surname
-    var userNameSurname = document.getElementById('user_name_surname').value;
-    console.log("userNameSurname set to: " + userNameSurname);
+    var userNameSurname1 = document.getElementById('user_name_surname_1').value;
+    var userNameSurname2 = document.getElementById('user_name_surname_2').innerHTML = userNameSurname1;
+    console.log("userNameSurname1 set to: " + userNameSurname1);
     // how much km do you want to travel?
     var travelKm = document.getElementById('travel_km').value;
     console.log("travelKm set to: " + travelKm);
@@ -14,14 +15,25 @@ btnGen.addEventListener('click',
     // ticket pricing 0.21 euros per km
     // < 18y 20% discount
     // > 65y 40% discount
-    var travelPrice;
+    // else 0% discount
+    var travelPrice = document.getElementById('travel_price');
+    var offers = document.getElementById('offers');
     if (userAge == 'minor') {
-      travelPrice = Math.ceil(travelKm * 0.21 * 0.80 * 100) / 100;
+      travelPrice.innerHTML = Math.ceil(travelKm * 0.21 * 0.80 * 100) / 100;
+      offers.innerHTML = 'Minor ticket';
     } else if (userAge == 'over65') {
-      travelPrice = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
+      travelPrice.innerHTML = Math.ceil(travelKm * 0.21 * 0.60 * 100) / 100;
+      offers.innerHTML = 'Over 65 ticket';
     } else {
-      travelPrice = Math.ceil(travelKm * 0.21 * 1.00 * 100) / 100;
+      travelPrice.innerHTML = Math.ceil(travelKm * 0.21 * 1.00 * 100) / 100;
+      offers.innerHTML = 'Standard ticket';
     }
     console.log("travelPrice set to: " + travelPrice);
+    // random wagon generator
+    var wagon = document.getElementById('wagon');
+    wagon.innerHTML = Math.floor(Math.random() * 10) + 1;
+    // random reservation code generator
+    var reservationCode = document.getElementById('reservation_code');
+    reservationCode.innerHTML = Math.floor(Math.random() * 100000) + 1;
   }
 );
