1ab1ccccc9580fd28032964b41ca62e95e78bd04
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-19 18:28:35 +0100

    Added display function

--- js/script.js
+++ js/script.js
@@ -28,12 +28,43 @@ btnGen.addEventListener('click',
       travelPrice.innerHTML = Math.ceil(travelKm * 0.21 * 1.00 * 100) / 100;
       offers.innerHTML = 'Standard ticket';
     }
-    console.log("travelPrice set to: " + travelPrice);
+    console.log("travelPrice set to: " + travelPrice.innerHTML);
     // random wagon generator
     var wagon = document.getElementById('wagon');
     wagon.innerHTML = Math.floor(Math.random() * 10) + 1;
     // random reservation code generator
     var reservationCode = document.getElementById('reservation_code');
     reservationCode.innerHTML = Math.floor(Math.random() * 100000) + 1;
+    // set ticket section display
+    var ticket = document.getElementById('ticket');
+    ticket.className = 'ticket display_block';
+  }
+);
+
+// canc button
+var btnCanc = document.getElementById('btn_canc');
+btnCanc.addEventListener('click',
+  function () {
+    // user name and surname
+    var userNameSurname1 = document.getElementById('user_name_surname_1').value = "";
+    var userNameSurname2 = document.getElementById('user_name_surname_2').innerHTML = "";
+    console.log("userNameSurname1 set to: " + userNameSurname1);
+    // km
+    var travelKm = document.getElementById('travel_km').value = '';
+    console.log("travelKm set to: " + travelKm);
+    // age
+    var userAge = document.getElementById('user_age').value = '';
+    console.log("userAge set to: " + userAge);
+    // ticket pricing
+    var travelPrice = document.getElementById('travel_price').innerHTML = "";
+    var offers = document.getElementById('offers').innerHTML = "";
+    console.log("travelPrice set to: " + travelPrice.innerHTML);
+    // wagon
+    var wagon = document.getElementById('wagon').innerHTML = "";
+    // reservation code
+    var reservationCode = document.getElementById('reservation_code').innerHTML = "";
+    // set ticket section display
+    var ticket = document.getElementById('ticket');
+    ticket.className = 'ticket display_none';
   }
 );
